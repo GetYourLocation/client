ab446a9c1b87d924a11d58d2936bff0137f5541f
Steven Liu <stevennl@foxmail.com>
2017-04-05 23:55:31 +0800

    Add VolleyPlus framework & http request demo

--- app/src/main/java/com/getyourlocation/app/gyl_client/TestActivity.java
+++ app/src/main/java/com/getyourlocation/app/gyl_client/TestActivity.java
@@ -2,51 +2,49 @@ package com.getyourlocation.app.gyl_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.error.AuthFailureError;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.getyourlocation.app.gyl_client.util.CommonUtil;
 import com.getyourlocation.app.gyl_client.util.SensorUtil;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class TestActivity extends AppCompatActivity {
     private static final String TAG = "TestActivity";
 
+    private static final String SERVER_IP = "http://192.168.199.105";
+    private static final String URL_SUM = SERVER_IP + "/gyl/api/sum";
+    private static final String URL_PRODUCT = SERVER_IP + "/gyl/api/product";
+
     private SensorUtil sensorUtil;
-    private TextView infoTxt;
+    private RequestQueue requestQueue;
+
+    private TextView sensorInfoTxt;
+    private EditText xValTxt;
+    private EditText yValTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
-        infoTxt = (TextView) findViewById(R.id.test_infoTxt);
-
-        Button resetBtn = (Button) findViewById(R.id.test_resetBtn);
-        resetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sensorUtil.reset();
-            }
-        });
-
-        sensorUtil = SensorUtil.getInstance(this);
-        sensorUtil.setOnSensorUpdatedListener(new SensorUtil.OnSensorUpdatedListener() {
-            @Override
-            public void onUpdated() {
-                float[] acceleration = sensorUtil.getAcceleration();
-                float[] gyroRotate = sensorUtil.getGyroRotate();
-                float compassRotate = sensorUtil.getCompassRotate();
-                infoTxt.setText(getString(R.string.sensor_data,
-                        acceleration[0],
-                        acceleration[1],
-                        acceleration[2],
-                        gyroRotate[0],
-                        gyroRotate[1],
-                        gyroRotate[2],
-                        compassRotate));
-            }
-        });
+        initReqQueue();
+        initSensor();
+        initSumProduct();
     }
 
     @Override
@@ -59,5 +57,118 @@ public class TestActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         sensorUtil.unregister();
+    }
+
+    private void initReqQueue() {
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+    }
+
+    private void initSensor() {
+        sensorInfoTxt = (TextView) findViewById(R.id.test_sensorInfoTxt);
+        Button resetBtn = (Button) findViewById(R.id.test_resetBtn);
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sensorUtil.reset();
+            }
+        });
+        sensorUtil = SensorUtil.getInstance(this);
+        sensorUtil.setOnSensorUpdatedListener(new SensorUtil.OnSensorUpdatedListener() {
+            @Override
+            public void onUpdated() {
+                float[] acceleration = sensorUtil.getAcceleration();
+                float[] gyroRotate = sensorUtil.getGyroRotate();
+                float compassRotate = sensorUtil.getCompassRotate();
+                sensorInfoTxt.setText(getString(R.string.sensor_data,
+                        acceleration[0],
+                        acceleration[1],
+                        acceleration[2],
+                        gyroRotate[0],
+                        gyroRotate[1],
+                        gyroRotate[2],
+                        compassRotate));
+            }
+        });
+    }
+
+    private void initSumProduct() {
+        xValTxt = (EditText) findViewById(R.id.test_x_editTxt);
+        yValTxt = (EditText) findViewById(R.id.test_y_editTxt);
+        Button sumBtn = (Button) findViewById(R.id.test_sumBtn);
+        Button productBtn = (Button) findViewById(R.id.test_productBtn);
+        sumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(xValTxt.getText().toString());
+                int y = Integer.parseInt(yValTxt.getText().toString());
+                computeSum(x, y);
+            }
+        });
+        productBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(xValTxt.getText().toString());
+                int y = Integer.parseInt(yValTxt.getText().toString());
+                computeProduct(x, y);
+            }
+        });
+    }
+
+    private void computeSum(final int x, final int y) {
+        requestQueue.add(new StringRequest(Request.Method.GET, URL_SUM, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response);
+                try {
+                    JSONObject jsonObj = new JSONObject(response);
+                    int ans = (int)jsonObj.get("ans");
+                    CommonUtil.showToast(TestActivity.this, String.valueOf(ans));
+                } catch (Exception e) {
+                    Log.e(TAG, "", e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "", error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("x", String.valueOf(x));
+                params.put("y", String.valueOf(y));
+                return params;
+            }
+        });
+    }
+
+    private void computeProduct(final int x, final int y) {
+        requestQueue.add(new StringRequest(Request.Method.POST, URL_PRODUCT, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response);
+                try {
+                    JSONObject jsonObj = new JSONObject(response);
+                    int ans = (int)jsonObj.get("ans");
+                    CommonUtil.showToast(TestActivity.this, String.valueOf(ans));
+                } catch (Exception e) {
+                    Log.e(TAG, "", e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "", error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("x", String.valueOf(x));
+                params.put("y", String.valueOf(y));
+                return params;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/getyourlocation/app/gyl_client/util/CommonUtil.java
@@ -0,0 +1,32 @@
+package com.getyourlocation.app.gyl_client.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+
+/**
+ * Commonly used methods.
+ */
+public class CommonUtil {
+    private static final String TAG = "CommonUtil";
+
+    /**
+     * Show a Toast message
+     * @param context The context
+     * @param strId The string resource id
+     */
+    public static void showToast(Context context, int strId) {
+        showToast(context, context.getString(strId));
+    }
+
+    /**
+     * Show a Toast message
+     * @param context The context
+     * @param str The string content
+     */
+    public static void showToast(Context context, String str) {
+        if (context != null) {
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
