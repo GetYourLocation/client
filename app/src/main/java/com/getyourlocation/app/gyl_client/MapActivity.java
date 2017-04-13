b82e11a195dd9f9029513ecdc1e75bc60a0ff8fc
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-04-13 22:22:00 +0800

    Add map activity

---
+++ app/src/main/java/com/getyourlocation/app/gyl_client/MapActivity.java
@@ -0,0 +1,15 @@
+package com.getyourlocation.app.gyl_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_map);
+//    }
+
+    // TODO Quickstart 里的内容直接粘贴到这边试试吧
+}
