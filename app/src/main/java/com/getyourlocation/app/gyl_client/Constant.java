91a877b08b5c69e28a56c47e587a009aab37c038
fengyifei <171064795@qq.com>
2017-04-18 13:54:01 +0800

    change key

--- app/src/main/java/com/getyourlocation/app/gyl_client/Constant.java
+++ app/src/main/java/com/getyourlocation/app/gyl_client/Constant.java
@@ -2,7 +2,7 @@ package com.getyourlocation.app.gyl_client;
 
 
 public class Constant {
-    public static final String APP_KEY = "ad57bb1be3394426a90224b439023deb";
+    public static final String APP_KEY = "ef51f2f3b4fa41f993a62e19a8dc3e08";
     public static final String LUR_NAME = "Nagrand/lua";
     public static final String URL_SERVER = "http://api.ipalmap.com/";
 }
