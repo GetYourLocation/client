780c220f9fe4ba135650032e56038c1644851ba0
Steven Liu <stevennl@foxmail.com>
2017-04-20 11:52:36 +0800

    Update cloud addr

--- app/src/main/java/com/getyourlocation/app/gyl_client/Constant.java
+++ app/src/main/java/com/getyourlocation/app/gyl_client/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
     public static final String LUR_NAME = "Nagrand/lua";
 
     public static final String URL_MAP_SERVER = "http://api.ipalmap.com/";
-    public static final String URL_CLOUD_SERVER = "http://ras.sysu.edu.cn:9312";
+    public static final String URL_CLOUD_SERVER = "http://ras.sysu.edu.cn:9512";
     public static final String URL_API_SUM = URL_CLOUD_SERVER + "/api/sum";
     public static final String URL_API_PRODUCT = URL_CLOUD_SERVER + "/api/product";
     public static final String URL_API_UPLOAD = URL_CLOUD_SERVER + "/api/upload";
