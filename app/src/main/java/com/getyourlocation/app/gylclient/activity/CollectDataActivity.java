1dce2d6b132d80a50d26cb0116afd6a48a4ea7d4
Lion3 <315966692@qq.com>
2017-05-03 22:37:22 +0800

    add cam preview

--- app/src/main/java/com/getyourlocation/app/gylclient/activity/CollectDataActivity.java
+++ app/src/main/java/com/getyourlocation/app/gylclient/activity/CollectDataActivity.java
@@ -1,17 +1,53 @@
 package com.getyourlocation.app.gylclient.activity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
+import com.getyourlocation.app.gylclient.CameraPreview;
 import com.getyourlocation.app.gylclient.R;
 
 
 public class CollectDataActivity extends AppCompatActivity {
 
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collect_data);
-        // TODO 数据采集页面
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
 }
