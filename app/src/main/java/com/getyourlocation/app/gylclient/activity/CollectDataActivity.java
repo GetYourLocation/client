ecbe396c3bd924bd5bc055bb7050d63b6677255a
Steven Liu <stevennl@foxmail.com>
2017-05-04 10:40:08 +0800

    Refactor CameraPreview: add auto focus and best preview size

--- app/src/main/java/com/getyourlocation/app/gylclient/activity/CollectDataActivity.java
+++ app/src/main/java/com/getyourlocation/app/gylclient/activity/CollectDataActivity.java
@@ -1,53 +1,91 @@
 package com.getyourlocation.app.gylclient.activity;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
-import com.getyourlocation.app.gylclient.CameraPreview;
+import com.getyourlocation.app.gylclient.util.CommonUtil;
+import com.getyourlocation.app.gylclient.util.SensorUtil;
+import com.getyourlocation.app.gylclient.widget.CameraPreview;
 import com.getyourlocation.app.gylclient.R;
 
 
 public class CollectDataActivity extends AppCompatActivity {
-
-
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-    private Camera mCamera;
-    private CameraPreview mPreview;
+    private static final String TAG = "CollectDataActivity";
+    private TextView sensorInfoTxt;
+    private SensorUtil sensorUtil;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collect_data);
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        initSensor();
+        initCamera();
+        initPreview();
+        initCaptureBtn();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorUtil.register();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorUtil.unregister();
+        releaseResources();
+    }
+
+    private void releaseResources() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private void initSensor() {
+        sensorInfoTxt = (TextView) findViewById(R.id.data_sensorTxt);
+        sensorUtil = SensorUtil.getInstance(this);
+        sensorUtil.setOnSensorUpdatedListener(new SensorUtil.OnSensorUpdatedListener() {
+            @Override
+            public void onUpdated() {
+                String txt = "Sensor: " + sensorUtil.getCompassRotate();
+                sensorInfoTxt.setText(txt);
+            }
+        });
+    }
+
+    private void initCamera() {
+        try {
+            camera = Camera.open();
+        } catch (Exception e){
+            Log.e(TAG, "", e);
+        }
+    }
+
+    private void initPreview() {
+        CameraPreview cameraPreview = new CameraPreview(this, camera);
+        FrameLayout layout = (FrameLayout) findViewById(R.id.data_preview_layout);
+        layout.addView(cameraPreview);
+    }
+
+    private void initCaptureBtn() {
+        Button captureBtn = (Button) findViewById(R.id.data_capture_btn);
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CommonUtil.showToast(CollectDataActivity.this, "Capture button clicked!");
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/getyourlocation/app/gylclient/widget/CameraPreview.java
@@ -0,0 +1,126 @@
+package com.getyourlocation.app.gylclient.widget;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+    private Camera camera;
+    private Camera.Size bestPreviewSize;
+
+    public CameraPreview(Context context) {
+        this(context, null);
+    }
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(this);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d(TAG, "onMeasure() called");
+        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        setMeasuredDimension(width, height);
+        List<Camera.Size> supportedSizes = camera.getParameters().getSupportedPreviewSizes();
+        if (supportedSizes != null) {
+            bestPreviewSize = getOptimalPreviewSize(supportedSizes, width, height);
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated() called");
+        try {
+            camera.setPreviewDisplay(holder);
+            Camera.Parameters params = camera.getParameters();
+            params.setPreviewSize(bestPreviewSize.width, bestPreviewSize.height);
+            if (params.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            } else {
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            }
+            camera.setParameters(params);
+            camera.startPreview();
+        } catch (IOException e) {
+            Log.e(TAG, "", e);
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceDestroyed() called");
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        Log.d(TAG, "surfaceChanged() called");
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (holder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (Exception e){
+            Log.e(TAG, "", e);
+        }
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+}
