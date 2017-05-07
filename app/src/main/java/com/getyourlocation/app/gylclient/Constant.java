72de78de39e505addb4c587638115220e5975399
Steven Liu <stevennl@foxmail.com>
2017-05-07 22:13:24 +0800

    Fix typos

--- app/src/main/java/com/getyourlocation/app/gylclient/Constant.java
+++ app/src/main/java/com/getyourlocation/app/gylclient/Constant.java
@@ -11,5 +11,5 @@ public class Constant {
     public static final String URL_API_SUM = URL_CLOUD_SERVER + "/api/sum";
     public static final String URL_API_PRODUCT = URL_CLOUD_SERVER + "/api/product";
     public static final String URL_API_UPLOAD = URL_CLOUD_SERVER + "/api/upload";
-    public static final String URL_API_GETLOCATION = URL_CLOUD_SERVER + "/api/localization";
+    public static final String URL_API_LOCALIZATION = URL_CLOUD_SERVER + "/api/localization";
 }
--- app/src/main/java/com/getyourlocation/app/gylclient/activity/LocalizationActivity.java
+++ app/src/main/java/com/getyourlocation/app/gylclient/activity/LocalizationActivity.java
@@ -6,55 +6,45 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.error.AuthFailureError;
 import com.android.volley.error.VolleyError;
-import com.android.volley.request.SimpleMultiPartRequest;
 import com.android.volley.request.StringRequest;
 
 import com.getyourlocation.app.gylclient.R;
 
-import com.android.volley.toolbox.ImageLoader;
 import com.getyourlocation.app.gylclient.Constant;
-import com.getyourlocation.app.gylclient.R;
 import com.getyourlocation.app.gylclient.util.CommonUtil;
 import com.getyourlocation.app.gylclient.util.NetworkUtil;
-import com.getyourlocation.app.gylclient.util.SensorUtil;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class LocalizationActivity extends AppCompatActivity {
     private static final String TAG = "LocalizationActivity";
-    private TextView sensorInfoTxt;
-    private EditText x1ValTxt;
-    private EditText y1ValTxt;
-    private EditText x2ValTxt;
-    private EditText y2ValTxt;
-    private EditText x3ValTxt;
-    private EditText y3ValTxt;
-    private EditText AlphaValTxt;
-    private EditText BetaValTxt;
-    private ImageButton imgBtn;
-    private ImageView resultImgView;
+
     private NetworkUtil networkUtil;
+
+    private EditText x1Txt;
+    private EditText y1Txt;
+    private EditText x2Txt;
+    private EditText y2Txt;
+    private EditText x3Txt;
+    private EditText y3Txt;
+    private EditText alphaTxt;
+    private EditText betaTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localization);
-        // TODO 定位
-        initData();
-        //initBtn();
-        //initResultImgView();
         initNetwork();
+        initData();
     }
 
     private void initNetwork() {
@@ -62,33 +52,34 @@ public class LocalizationActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        x1ValTxt = (EditText) findViewById(R.id.test_x1_editTxt);
-        y1ValTxt = (EditText) findViewById(R.id.test_y1_editTxt);
-        x2ValTxt = (EditText) findViewById(R.id.test_x2_editTxt);
-        y2ValTxt = (EditText) findViewById(R.id.test_y2_editTxt);
-        x3ValTxt = (EditText) findViewById(R.id.test_x3_editTxt);
-        y3ValTxt = (EditText) findViewById(R.id.test_y3_editTxt);
-        AlphaValTxt = (EditText) findViewById(R.id.test_alpha_editTxt);
-        BetaValTxt = (EditText) findViewById(R.id.test_beta_editTxt);
+        x1Txt = (EditText) findViewById(R.id.test_x1_editTxt);
+        y1Txt = (EditText) findViewById(R.id.test_y1_editTxt);
+        x2Txt = (EditText) findViewById(R.id.test_x2_editTxt);
+        y2Txt = (EditText) findViewById(R.id.test_y2_editTxt);
+        x3Txt = (EditText) findViewById(R.id.test_x3_editTxt);
+        y3Txt = (EditText) findViewById(R.id.test_y3_editTxt);
+        alphaTxt = (EditText) findViewById(R.id.test_alpha_editTxt);
+        betaTxt = (EditText) findViewById(R.id.test_beta_editTxt);
         Button ResultBtn = (Button) findViewById(R.id.GetLocationBtn);
         ResultBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int x1 = Integer.parseInt(x1ValTxt.getText().toString());
-                int y1 = Integer.parseInt(y1ValTxt.getText().toString());
-                int x2 = Integer.parseInt(x2ValTxt.getText().toString());
-                int y2 = Integer.parseInt(y2ValTxt.getText().toString());
-                int x3 = Integer.parseInt(x3ValTxt.getText().toString());
-                int y3 = Integer.parseInt(y3ValTxt.getText().toString());
-                float alpha = Integer.parseInt(AlphaValTxt.getText().toString());
-                float beta = Integer.parseInt(BetaValTxt.getText().toString());
+                int x1 = Integer.parseInt(x1Txt.getText().toString());
+                int y1 = Integer.parseInt(y1Txt.getText().toString());
+                int x2 = Integer.parseInt(x2Txt.getText().toString());
+                int y2 = Integer.parseInt(y2Txt.getText().toString());
+                int x3 = Integer.parseInt(x3Txt.getText().toString());
+                int y3 = Integer.parseInt(y3Txt.getText().toString());
+                float alpha = Integer.parseInt(alphaTxt.getText().toString());
+                float beta = Integer.parseInt(betaTxt.getText().toString());
                 GetLocation(x1, y1, x2, y2, x3, y3, alpha, beta);
             }
         });
     }
+
     private void GetLocation(final int x1, final int y1, final int x2, final int y2,
                              final int x3, final int y3, final float alpha, final  float beta) {
-        StringRequest req = new StringRequest(Request.Method.GET, Constant.URL_API_GETLOCATION,
+        StringRequest req = new StringRequest(Request.Method.GET, Constant.URL_API_LOCALIZATION,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {  // Called when server respond
@@ -97,7 +88,7 @@ public class LocalizationActivity extends AppCompatActivity {
                             JSONObject jsonObj = new JSONObject(response);
                             double x = (double)jsonObj.get("x");
                             double y = (double)jsonObj.get("y");
-                            CommonUtil.showToast(LocalizationActivity.this, String.valueOf("x:" + x + "y:" + y ));
+                            CommonUtil.showToast(LocalizationActivity.this, String.valueOf("x:" + x + " y:" + y ));
                         } catch (Exception e) {
                             Log.e(TAG, "", e);
                         }
