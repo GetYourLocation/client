f97610133e15cb89c2a87db1e42ca984de1d7911
fengyifei <171064795@qq.com>
2017-05-07 22:33:05 +0800

    修改了CollectDataActivity.java，收集了与每帧相对应的Compass数据

--- app/src/main/java/com/getyourlocation/app/gylclient/activity/CollectDataActivity.java
+++ app/src/main/java/com/getyourlocation/app/gylclient/activity/CollectDataActivity.java
@@ -19,9 +19,15 @@ import com.getyourlocation.app.gylclient.util.SensorUtil;
 import com.getyourlocation.app.gylclient.widget.CameraPreview;
 import com.getyourlocation.app.gylclient.R;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.ArrayList;
 
 
 public class CollectDataActivity extends AppCompatActivity {
@@ -43,6 +49,8 @@ public class CollectDataActivity extends AppCompatActivity {
     private boolean isRecording = false;
     private int seconds = 0;
     private int frameCnt = 1;
+
+    private ArrayList<Float> compassData = new ArrayList<Float>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,6 +118,8 @@ public class CollectDataActivity extends AppCompatActivity {
             public void onPreviewFrame(byte[] data, Camera camera) {
                 if (isRecording) {
                     storeFrame(data);
+                    //记录该帧的罗盘数据
+                    compassData.add(Float.valueOf(sensorUtil.getCompassRotate()));
                 }
             }
         });
@@ -127,14 +137,33 @@ public class CollectDataActivity extends AppCompatActivity {
                     frameCnt = 1;
                     handler.post(timingRunnable);
                     isRecording = true;
+                    if (!compassData.isEmpty())
+                        compassData.clear();
                 } else {
                     handler.removeCallbacks(timingRunnable);
                     recordBtn.setText("Start");
                     isRecording = false;
                     infoTxt.setText("Video saved at " + storageDir.getPath());
+                    storeCompassData();
                 }
             }
         });
+    }
+
+    private void storeCompassData() {
+        String filename = storageDir.getPath() + File.separator + "compassData.txt";
+        File compassDataFile = new File(filename);
+        try {
+            FileWriter fos = new FileWriter(compassDataFile);
+            for (Float f: compassData) {
+                fos.write(f.toString());
+                fos.write('\n');
+            }
+            fos.close();
+            Log.d(TAG, "compassData " + frameCnt + " saved");
+        } catch (Exception e) {
+            Log.e(TAG, "", e);
+        }
     }
 
     private void storeFrame(byte[] raw) {
