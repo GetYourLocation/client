d0f85b3503882a9611fb7fb6513cda5e4bbefa97
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-06-27 00:12:22 +0800

    Implement dynamic permissions

--- app/src/main/java/com/getyourlocation/app/client/util/PermissionUtil.java
+++ app/src/main/java/com/getyourlocation/app/client/util/PermissionUtil.java
@@ -3,25 +3,34 @@ package com.getyourlocation.app.client.util;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 
-
+/**
+ * Handle permissions dynamically.
+ */
 public class PermissionUtil {
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
+    private static final int REQ_PERMISSION = 1;
+    private static String[] PERMISSIONS = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
     };
 
-    public static void verifyStoragePermissions(Activity activity) {
-        int permission = ActivityCompat.checkSelfPermission(activity,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permission != PackageManager.PERMISSION_GRANTED) {  // No permission
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
+    public static void grantPermissions(Activity act) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            boolean allGranted = true;
+            for (String pms : PERMISSIONS) {
+                if (ActivityCompat.checkSelfPermission(act, pms)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    allGranted = false;
+                    break;
+                }
+            }
+            if (!allGranted) {
+                ActivityCompat.requestPermissions(act, PERMISSIONS, REQ_PERMISSION);
+            }
         }
     }
 }
